b1d5526690d2baf738d5b3e14297dc47ecdd3b4a
PopAdmin <maksim.biryukov.2008@gmail.com>
2025-10-14 17:15:35 +0500

    new-3.0

---
+++ web-kole/proj-13/main.js
@@ -0,0 +1,80 @@
+console.log("----------------------------");
+
+console.log("Сәлем!");
+console.info('Хабар');
+console.error('Қате');
+console.warn("Warnong");
+
+// -------------------------------------- //
+console.log("----------------------------");
+
+
+console.log("5 + 5 =", 5 + 5);
+console.log("12 - 6 =", 12 - 6);
+console.log("2 * 5 =", 2 * 5);
+console.log("9 / 4 =", 9 / 4);
+console.log("5 % 2 =", 5 % 2);
+
+let a = 1;
+var b = 2;
+const c = 3;
+
+console.log(`a + b = ${a + b}`)
+
+// -------------------------------------- //
+console.log("----------------------------");
+
+
+var num_1 = 5;
+var num_2 = 10;
+
+console.log("Итог: " + (num_1 + num_2));
+
+// -------------------------------------- //
+
+let a1 = 5;
+let a2 = 5;
+
+let sum1 = a1 + a2;
+let sum2 = a1 - a2;
+let sum3 = a1 * a2;
+let sum4 = a1 / a2;
+let sum5 = a1 % a2;
+
+// -------------------------------------- //
+console.log("----------------------------");
+
+console.log(`${a1} + ${a2} = ${sum1}`);
+console.log(`${a1} - ${a2} = ${sum2}`);
+console.log(`${a1} * ${a2} = ${sum3}`);
+console.log(`${a1} / ${a2} = ${sum4}`);
+console.log(`${a1} % ${a2} = ${sum5}`);
+
+// -------------------------------------- //
+console.log("----------------------------");
+
+var num1 = 12;
+var num2 = 10;
+
+var result1 = num1 + num2;
+var result2 = num1 - num2;
+var result3 = num1 * num2;
+var result4 = num1 / num2;
+var result5 = num1 % num2;
+
+console.log("Итог " + (result1));
+console.log("Итог " + (result2));
+console.log("Итог " + (result3));
+console.log("Итог " + (result4));
+console.log("Итог " + (result5));
+
+// -------------------------------------- //
+console.log("----------------------------");
+
+var sum11 = 5;
+sum11+=5;
+console.log("5 + 5 = " + (sum11));
+
+var sum22 = 6;
+sum22 = sum22 * 5;
+console.log("6 * 5 = " + (sum22));
