a41620fdd613305f9243807c4bc7b510fa0d9dd3
PopAdmin <maksim.biryukov.2008@gmail.com>
2025-10-15 15:47:22 +0500

    Proj-14

---
+++ web-kole/proj-14/ff.php
@@ -0,0 +1,20 @@
+<?php 
+    $x = 5;
+    $y = 2;
+    $z = null;
+
+    $sum1 = $x + $y;
+    $sum2 = $x - $y;
+    $sum3 = $x * $y;
+    $sum4 = $x / $y;
+    $sum5 = $x ** $y;
+    $sum6 = $x % $y;
+    
+    echo "$x + $y = $sum1 <br>";
+    echo "$x - $y = $sum2 <br>";
+    echo "$x * $y = $sum3 <br>";
+    echo "$x / $y = $sum4 <br>";
+    echo "$x ** $y = $sum5 <br>";
+    echo "$x % $y = $sum6 <br>";
+    echo "z = $z <- нөлге тен<br>";
+?>
