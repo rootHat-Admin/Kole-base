ff50f2b8dd07d70daabc5f8ffa978ac3cfec8fa8
Skynet <skynet@example.com>
2025-10-29 16:20:02 +0500

    t.php

---
+++ web-kole/proj-17/Exercise/t.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="kk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> "Тіркелу" формасы</title>
+    <style>
+        * {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        .text-red {
+            color: red;
+        }
+
+        .text-green {
+            color: green;
+        }
+    </style>
+</head>
+<body>
+     <form action="t.php" method="POST">
+        <label for="full_name">Аты-жөні: </label> <br>
+        <input type="text" name="full_name" required> <br><br>
+
+        <label for="username">Логин: </label> <br>
+        <input type="text" name="username" required> <br><br>
+
+        <label for="email">Пошта: </label> <br>
+        <input type="email" name="email" required> <br><br>
+
+        <label for="password">Құпиясөз: </label> <br>
+        <input type="password" name="password" required> <br><br>
+
+        <button type="submit">Тіркелу</button>
+    </form>
+
+    <?php 
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        $full_name = trim($_POST['full_name']);
+        $username = trim($_POST['username']);
+        $email = trim($_POST['email']);
+        $password = trim($_POST['password']);
+
+        // Тексерушарттары
+        if (empty($full_name) || empty($username) || empty($email) || empty($password)) { // барлық өріс толтырылған
+            echo "Өрістерді толтырыңыз!";
+        } else { 
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)) { // пошта форматы дұрыс
+                echo '<br><hr> <h3 class="text-red">Жарамсыз email мекенжайы! </h3>'; // Қате болса - қызыл түспен хабарлама
+            } elseif (strlen($password) < 6) { //  құпиясөз кемінде 6 таңбалы.
+                echo '<br><hr> <h3 class="text-red">Құпия сөз кемінде 6 таңбадан тұруы керек.</h3>'; // Қате болса - қызыл түспен хабарлама
+            } else {
+                echo '<br><hr> <h3 class="text-green">Тіркелу сәтті өтті!</h3>'; // Дұрыс енгізілгенде - "Тіркелусәтті өтті!" 
+                echo "<p>Аты-жөні: " . htmlspecialchars($full_name) . "</p>";
+                echo "<p>Логин: " . htmlspecialchars($username) . "</p>";
+                echo "<p>Пошта: " . htmlspecialchars($email) . "</p>";
+                echo "<p>Құпиясөз: " . htmlspecialchars($password) . "</p>";
+            }
+        }
+
+    }
+    ?>
+
+</body>
+</html>
+
--- web-kole/proj-17/p1.php
+++ web-kole/proj-17/p1.php
@@ -6,7 +6,7 @@
     <title>1</title>
 </head>
 <body>
-    <form action="php" method="post">
+    <form action="p1.php" method="POST">
         <label for="name">Name: </label> <br>
         <input type="text" name="name"> <br><br>
 
@@ -27,12 +27,18 @@
         $email = trim($_POST['email']);
 
         // Бос мәтіндерді тексеру
-
         if (empty($name) || empty($age) || empty($email)) {
             echo "Өрістерді толтырыңыз!";
-        } else {
+        } else { // Age Тексеру
             if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
                 echo "Жарамсыз email мекенжайы!";
+            } elseif (!is_numeric($age) || $age < 5) {
+                echo "Жасыңыз 5-тен кем болмауы керек!";
+            } else {
+                echo "<h1> Malimet satti kad </h1>";
+                echo "<p>Name: " . htmlspecialchars($name) . "</p>";
+                echo "<p>Age: " . htmlspecialchars($age) . "</p>";
+                echo "<p>Email: " . htmlspecialchars($age) . "</p>";
             }
         }
 
