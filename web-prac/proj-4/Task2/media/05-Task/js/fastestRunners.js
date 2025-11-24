afe964675c2c562e41a86dc975426e6a491b6d9c
Skynet <skynet@example.com>
2025-11-24 09:59:28 +0500

    test2

---
+++ web-prac/proj-4/Task2/media/05-Task/js/fastestRunners.js
@@ -0,0 +1,18 @@
+/**
+ * @typedef {Object} Runner
+ * @property {String} name
+ * @property {String[]} paces
+ * @typedef {Object} Result
+ * @property {String} name
+ * @property {String} averagePace
+ * @property {String} fastestPace
+ */
+
+/**
+ * @param {Runner[]} runners
+ * @return {Result[]}
+ */
+
+function fastestRunners(runners) {
+	/* Work here */
+}
