8e3e1d68aa5bc872b51b010346660a91de77c889
Skynet <skynet@example.com>
2025-11-25 09:14:35 +0500

    Task5

--- web-prac/proj-4/Task2/media/05-Task/js/fastestRunners.js
+++ web-prac/proj-4/Task2/media/05-Task/js/fastestRunners.js
@@ -14,5 +14,51 @@
  */
 
 function fastestRunners(runners) {
-	/* Work here */
+	// темп "M:SS" форматындағы мәнді секундқа ауыстыру
+	const toSec = pace => {
+		const [m, s] = pace.split(":").map(Number);
+		return m * 60 + s;
+	};
+
+	// секундтарды қайтадан "M:SS" форматына ауыстыру
+	const toPace = sec => {
+		const m = Math.floor(sec / 60);
+		const s = Math.round(sec % 60);
+		return `${m}:${String(s).padStart(2,"0")}`;
+	};
+
+	// әрбір жүгірушінің жалпы уақытын (totalTime) қосу
+	const withTotal = runners.map(r => {
+		const totalTime = r.paces.reduce((sum, p) => sum + toSec(p), 0);
+		return {...r, totalTime};
+	});
+
+	// барлық жүгірушілердің орташа уақытын есептеу
+	const avgTotal = withTotal.reduce((sum, r) => sum + r.totalTime, 0) / withTotal.length;
+
+	// орташа уақыттан тезірек жүгіргендерді сүзгіден өткізіп, уақыт бойынша сұрыптау
+	const faster = withTotal
+		.filter(r => r.totalTime < avgTotal)
+		.sort((a, b) => a.totalTime - b.totalTime);
+		
+	// нәтижелік массивті құру
+	return faster.map(r => {
+		const avgPace = toPace(r.totalTime / r.paces.length); // орташа темп
+		const fastestPace = r.paces.reduce((f, c) => toSec(c) < toSec(f) ? c : f); // ең жылдам темп
+		return {name: r.name, averagePace: avgPace, fastestPace};
+	});
 }
+
+// функциядан кейін: JSON файлын жүктеу және нәтижені HTML-ге шығару
+fetch("js/runners.json")
+.then(res => res.json()) // JSON-ды массивке ауыстыру
+.then(data => {
+    const fastest = fastestRunners(data);  // тезірек жүгіргендерді алу
+    const fastestEl = document.getElementById("fastest"); // HTML элементін таңдау
+    fastestEl.innerHTML = `
+        <strong>Ең жылдам жүгірушілер:</strong>
+        <ul>
+        ${fastest.map(r => `<li>${r.name} - орташа қарқын: ${r.averagePace}, ең жылдам: ${r.fastestPace}</li>`).join("")}
+        </ul>
+    `;
+});
